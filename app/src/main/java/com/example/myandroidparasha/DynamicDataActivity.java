13ee5ad5f92620e09a0e41b34a9bce4548f22afb
VladimirIAlexandrov <90275956+VladimirIAlexandrov@users.noreply.github.com>
2023-09-11 14:15:27 +0300

    first commit
    Signed-off-by: VladimirIAlexandrov <90275956+VladimirIAlexandrov@users.noreply.github.com>

---
+++ app/src/main/java/com/example/myandroidparasha/DynamicDataActivity.java
@@ -0,0 +1,117 @@
+package com.example.myandroidparasha;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.AdapterView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class DynamicDataActivity extends AppCompatActivity {
+
+    private EditText dataEditText;
+    private Button addButton;
+    private ListView listView;
+    private TextView userNameTextView;
+    private ArrayList<String> dataList;
+    private ArrayAdapter<String> adapter;
+
+    private String userName;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        userName = getIntent().getStringExtra("userName");
+        userNameTextView.setText(userName);
+        userNameTextView.setTextColor(Color.GRAY);
+        userNameTextView.setTextScaleX(1.5F);
+    }
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic_data);
+        userNameTextView = findViewById(R.id.nameTextView);
+        dataEditText = findViewById(R.id.dataEditText);
+        addButton = findViewById(R.id.addButton);
+        listView = findViewById(R.id.listView);
+
+        dataList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dataList);
+        listView.setAdapter(adapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = dataEditText.getText().toString();
+
+                if (!data.isEmpty()) {
+                    dataList.add(data);
+                    adapter.notifyDataSetChanged();
+                    dataEditText.setText("");
+
+                    Log.i("msg","Запись успешно добавлена");
+
+                    Toast mytoast = new Toast(DynamicDataActivity.this);
+                    Toast.makeText(DynamicDataActivity.this, "Запись успешно добавлена", Toast.LENGTH_SHORT).show();
+                }else {
+                    Log.e("Ошибка","данные не введены");
+                    Toast mytoast = new Toast(DynamicDataActivity.this);
+                    Toast.makeText(DynamicDataActivity.this, "Вы не ввели текст", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                showDeleteDialog(position);
+            }
+        });
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        dataList.clear();
+        adapter.clear();
+    }
+
+    private void showDeleteDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Удаление элемента")
+                .setMessage("Вы уверены, что хотите удалить этот элемент?")
+                .setPositiveButton("Удалить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dataList.remove(position);
+
+                        adapter.notifyDataSetChanged();
+
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        Dialog dialog = builder.create();
+        dialog.show();
+    }
+}
--- app/src/main/java/com/example/myandroidparasha/MainActivity.java
+++ app/src/main/java/com/example/myandroidparasha/MainActivity.java
@@ -1,74 +1,96 @@
 package com.example.myandroidparasha;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
-import android.graphics.Color;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myandroidparasha.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    private Map<String, String> loginCredentials = new HashMap<>();
 
-    private int counter = 0;
-    private TextView counterTextView;
-    private Button incrementButton1;
-    private Button incrementButton2;
-    private Button incrementButton3;
+    private EditText nameEditText;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
 
-    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int color = Color.parseColor("#349188");
-        ActionBar actionBar = getSupportActionBar(); // Получите ActionBar
-        if (actionBar != null) {
-            actionBar.setTitle("Laba1");
-        }
+        nameEditText = findViewById(R.id.nameEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        Button loginButton = findViewById(R.id.loginButton);
 
-
-
-        counterTextView = findViewById(R.id.textView3);
-        incrementButton1 = findViewById(R.id.button);
-        incrementButton2 = findViewById(R.id.button2);
-        incrementButton3 = findViewById(R.id.button3);
-
-        incrementButton1.setBackgroundColor(color);
-        incrementButton2.setBackgroundColor(color);
-        incrementButton3.setBackgroundColor(color);
-
-        incrementButton1.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                counter++;
-                updateCounter();
-            }
-        });
+                String enteredname = nameEditText.getText().toString();
+                String enteredUsername = usernameEditText.getText().toString();
+                String enteredPassword = passwordEditText.getText().toString();
+                readLoginCredentials();
+                if (loginCredentials.containsKey(enteredUsername) && loginCredentials.get(enteredUsername).equals(enteredPassword) && !enteredname.isEmpty()) {
 
-        incrementButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter++;
-                updateCounter();
-            }
-        });
+                    Intent intent = new Intent(MainActivity.this, DynamicDataActivity.class);
+                    intent.putExtra("userName", enteredname);
+                    startActivity(intent);
 
-        incrementButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                counter = 0;
-                updateCounter();
+
+                    Log.i("msg","Авторизация успешна");
+                    Toast mytoast = new Toast(MainActivity.this);
+                    Toast.makeText(MainActivity.this, "Авторизация успешна", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Log.e("Ошибка","Ошибка авторизации");
+
+                    Toast mytoast = new Toast(MainActivity.this);
+                    Toast.makeText(MainActivity.this, "Ошибка авторизации", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
 
-    private void updateCounter() {
-        counterTextView.setText("Счетчик: " + counter);
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+    private void readLoginCredentials() {
+        Resources resources = getResources();
+        InputStream inputStream = resources.openRawResource(R.raw.logins_passwords);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                // Парсинг строки в логин и пароль
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    loginCredentials.put(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
